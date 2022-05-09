5bd1d997b7de4a1d5d544573059e0563394608b3
Eugenio <euggnio@gmail.com>
2022-05-09 11:42:58 -0300

    second

--- src/escalonamento/Processador.java
+++ src/escalonamento/Processador.java
@@ -4,15 +4,20 @@ import java.util.ArrayList;
 
 public class Processador {
 
-    int tempo , aux, aux2;
+    int tempo , aux ;
+    float aux2;
 
     String temp = "Tempo em execução:    ";
     String rest = "Tempo restante Proc:   ";
     String proc = "Processo em execução: ";
+    String procespera = "Tempo de espera do processo: ";
+    String procfila = "IDS dos Processos e seus tempos de execução: \n";
 
     public void processar(ArrayList<Processos> fila){
-
         for (int i = 0; i < fila.size(); i++) {
+            procespera = procespera + "id"+ fila.get(i).id + " " + tempo + "s, ";
+            procfila = procfila + "ID:" + fila.get(i).id + " Temp. de Exe.: " + fila.get(i).tempo_execucao + "s | ";
+            aux2 = aux2 + (tempo);
 
             for (int j = 0; j < fila.get(i).tempo_execucao; j++) {
                 if(tempo <=10) {
@@ -28,18 +33,37 @@ public class Processador {
                     rest = rest + "|" + fila.get(i).tempo_restante;
                 }
                 proc = proc +" |"+ fila.get(i).id;
-
-                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                System.out.println(temp);
-                System.out.println(rest);
-                System.out.println(proc);
                 fila.get(i).tempo_restante--;
                 tempo++;
             }
 
+
         }
+        
+        
+        
+       
+       System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println(procfila +"\n\n\n\n");
+       System.out.println("Resultado;\n" + temp);
+       System.out.println(rest);
+       System.out.println(proc);
+        
+        aux2 = aux2 / fila.size();
+        System.out.println(procespera);
+        System.out.printf("Tempo médio de espera: %.2f ", aux2);
+       
 
 
     }
 
 }
+
+    
+
+    
+    
+    
+    
+    
+
--- src/escalonamento/Processos.java
+++ src/escalonamento/Processos.java
@@ -2,10 +2,26 @@ package escalonamento;
 
 public class Processos {
 
-    int id;
-    int tempo_espera;
-    int tempo_execucao;
-    int tempo_restante;
+        int id;
+        int tempo_espera;
+        int tempo_execucao;
+        int tempo_restante;
+
+        public Processos(){
+            
+        }
+        
+        public Processos(int id, int tempo_espera, int tempo_execucao, int tempo_restante) {
+        this.id = id;
+        this.tempo_espera = tempo_espera;
+        this.tempo_execucao = tempo_execucao;
+        this.tempo_restante = tempo_restante;
+        }
+
+
+        
+        
+        
 
 
 }
